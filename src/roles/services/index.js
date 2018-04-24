0f08b08e105b281985b191189e0275cdb2e3b986
Raz Shuty <raz.shuty@wikimedia.de>
2018-04-24 15:14:55 +0200

    fixing the services.js

--- src/roles/services/index.js
+++ src/roles/services/index.js
@@ -5,19 +5,21 @@ const AbstractBaseRole = require( '../Base.js');
 class ServicesRole extends AbstractBaseRole {
     modifySetup( dockerComposeSetup, setupVars) {
         var dockerComposeConfig = this.extendObjectWithYaml( dockerComposeSetup, path.join( __dirname, 'docker-compose-build.yml' ) );
-        dockerComposeConfig.version = setupVars.serviceVersion;
-        dockerComposeConfig.services.wikibase.ports = [setupVars.dockerPort + ':80'];
-        return dockerComposeConfig;
+        return this.transformCompose(dockerComposeConfig, setupVars);
 
     }
     modifyServices( dockerCompose, setupVars) {
         var dockerComposeConfig = this.extendObjectWithYaml( dockerCompose, path.join( __dirname, 'docker-compose.yml' ) );
+        return this.transformCompose(dockerComposeConfig, setupVars);
+
+    }
+
+    transformCompose(dockerComposeConfig, setupVars){
         dockerComposeConfig.version = setupVars.serviceVersion;
         dockerComposeConfig.services.wikibase.ports = [setupVars.dockerPort + ':80'];
         return dockerComposeConfig;
 
     }
-
     // TODO copy Localsettings and other files from https://github.com/addshore/mediawiki-dev-tools, maybe even bootstrap the whole thing here
 }
 
